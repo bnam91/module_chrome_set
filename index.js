d77cb9f8eb2723377500e1c7fba0e33a252f49aa
bnam <darling_91@naver.com>
2026-01-15 21:01:33 +0900

    모듈화: npm 패키지로 사용 가능하도록 구조 개선

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * module-chrome-set
+ * 크롬 브라우저 자동 실행 모듈
+ * 
+ * 이 모듈은 Puppeteer를 사용하여 크롬 브라우저를 프로필별로 실행하고 관리합니다.
+ */
+
+const { openCoupang } = require('./src/browser/chrome-profile');
+
+// 메인 함수 export
+module.exports = {
+  openCoupang,
+};
+
+// 기본 export로도 사용 가능하도록
+module.exports.default = openCoupang;
